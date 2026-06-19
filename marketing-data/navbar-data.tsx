56b91e8cf53a44e732b2685ad1dedae2079ae501
ispasdani <ispasdani14@gmail.com>
2026-06-19 23:47:24 +0200

    navbar-data

---
+++ marketing-data/navbar-data.tsx
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    title: "Pricing",
+    href: "/pricing",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Careers",
+    href: "/careers",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+];
