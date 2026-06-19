2d5bcdd2aec78eb1bd8c1bc00817ae4db442a42e
ispasdani <ispasdani14@gmail.com>
2026-06-19 23:33:32 +0200

    marketing structure

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Home() {
-  return (
-    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      Here
-    </main>
-  );
-}
