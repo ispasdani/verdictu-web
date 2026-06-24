ae48bd586a35cd329a489b26ccbca32237e876dd
ispasdani14 <ispasdani14@gmail.com>
2026-06-24 18:24:23 +0200

    styling updates

--- components/marketing-general/navbars/desktop-nav.tsx
+++ components/marketing-general/navbars/desktop-nav.tsx
@@ -11,17 +11,24 @@ export const DesktopNav = ({
 }) => {
   return (
     <div className="hidden h-[8vh] w-full items-center justify-between px-4 py-4 md:flex">
-      <Logo />
       <div className="flex items-center gap-10">
+        <Logo />
         {items.map((item) => (
           <Link href={item.href} key={item.title} className="group">
-            <Text size="sm" color="muted" weight="light" hoverColor="default">
-              {item.title}
-            </Text>
+            <Button variant="link">
+              <Text size="sm" color="muted" weight="light" hoverColor="default">
+                {item.title}
+              </Text>
+            </Button>
           </Link>
         ))}
       </div>
       <div className="flex items-center gap-2">
+        <Button variant="secondary">
+          <Text size="sm" color="default" weight="light">
+            Download for MacOS
+          </Text>
+        </Button>
         <Button variant="primary">
           <Text size="sm" color="inverted" weight="light">
             Get started
