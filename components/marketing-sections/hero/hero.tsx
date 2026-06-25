5ec48f0971a8ca2d098250598818a65acb9f82bc
ispasdani <ispasdani14@gmail.com>
2026-06-25 16:31:49 +0200

    hero in the making

--- components/marketing-sections/hero/hero.tsx
+++ components/marketing-sections/hero/hero.tsx
@@ -13,19 +13,21 @@ const Herobox = () => {
           Dangerous to Everyone Else.
         </Text>
         {/* <Text size={"md"} >We are a team of passionate developers, designers, and marketers who are dedicated to creating the best possible experience for our users. We believe that the best way to achieve this is by putting our users first and foremost in everything we do.</Text> */}
-        <Text size={"xl"} className="mt-10 mb-5 max-w-[40vw]">
+        <Text size={"md"} className="mt-10 mb-5 max-w-[40vw]">
           Like the most loyal dog you've ever had — except it reads case law,
           writes your contracts, ranks your content, and remembers every
           conversation you've ever had. It stays. It learns. It never turns on
           you.
         </Text>
         <Button variant="primary">
-          <Text size="md" color="inverted" weight="light">
+          <Text size="sm" color="inverted" weight="light">
             Try it now
           </Text>
         </Button>
       </div>
-      <div className="min-h-[57vh] bg-green-800"></div>
+      <div className="min-h-[57vh] bg-green-800 flex justify-center items-center">
+        <div className="w-[90%] h-[45vh] bg-red-300 rounded-md"></div>
+      </div>
     </div>
   );
 };
