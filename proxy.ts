e268d7cd0fbbc8ee2ddedc07f8c040e11ab2b2c3
ispasdani <ispasdani14@gmail.com>
2026-07-15 23:14:04 +0200

    make /articles public in clerk middleware

--- proxy.ts
+++ proxy.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/", // homepage
+  "/articles(.*)",
   "/sign-in(.*)",
   "/sign-up(.*)",
 ]);
